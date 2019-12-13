35a8aa142befd865c518eb10e421f8082947766a
Rosen Vladimirov <vladimirov.rosen@gmail.com>
2019-12-13 14:17:39 +0200

    Re-organised

--- product_pricelist_competitor/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import api, fields, models, tools, _
-
-class Partner(models.Model):
-    _inherit = 'res.partner'
-
-    competitor = fields.Boolean('Competitor')
